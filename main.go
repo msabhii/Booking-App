8c5e9077852c15947f2f5494625b844ac9430d4f
Abhishek <msabhithakur7777@gmail.com>
2023-11-11 16:35:15 +0530

    First Go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	
+	var name = "Go Confrence"
+	const noOfTickets=50
+	var remainingTickets = 50
+	fmt.Println("Welcome to",name,"Booking Applicatin")
+	fmt.Println("Total Tickets ",noOfTickets,"Tickets Left",remainingTickets )
+	fmt.Println("Get your tickets Now!!")
+
+	var userName string
+	fmt.Println(userName)
+}
